6b9c2a617b89323ba8f087e68b5ebece853dd169
Hendrik Gani <hendrik.gani@gmail.com>
2019-02-11 18:44:34 +1100

    add icon troubleshooting info

--- ganilib-core/src/main/java/com/gani/lib/ui/icon/GIcon.kt
+++ ganilib-core/src/main/java/com/gani/lib/ui/icon/GIcon.kt
@@ -2,6 +2,9 @@ package com.gani.lib.ui.icon
 
 import com.mikepenz.iconics.IconicsDrawable
 
+// NOTE: Call the following line when proguard or applicationId mismatch prevents the auto detection of the fonts.
+//       From https://github.com/mikepenz/Android-Iconics/issues/234
+// Iconics.registerFont(GoogleMaterial())
 interface GIcon {
     fun drawable(): IconicsDrawable
 
