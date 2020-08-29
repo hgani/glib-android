ce71f609bba28acc3cbdaf5847c372a054000c32
Hendrik Gani <hendrik.gani@gmail.com>
2020-08-29 16:38:06 +1000

    Display basic notification

--- glib-core/src/main/java/com/glib/core/notification/Notifications.kt
+++ glib-core/src/main/java/com/glib/core/notification/Notifications.kt
@@ -1,6 +1,8 @@
 package com.glib.core.notification
 
+import android.app.Notification
 import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
 import androidx.annotation.RequiresApi
@@ -23,14 +25,22 @@ object Notifications {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) createNotificationChannel(channelId)
     }
 
+    fun display(id: Int, notification: Notification) {
+        manager().notify(id, notification)
+    }
+
     @RequiresApi(Build.VERSION_CODES.O)
     private fun createNotificationChannel(channelId: String) {
         val name = CHANNEL_NAME
         val description = CHANNEL_DESCRIPTION
-        val importance = android.app.NotificationManager.IMPORTANCE_HIGH
+        val importance = NotificationManager.IMPORTANCE_HIGH
         val mChannel = NotificationChannel(channelId, name, importance)
         mChannel.description = description
-        val mNotificationManager = Res.context.getSystemService(Context.NOTIFICATION_SERVICE) as android.app.NotificationManager
-        mNotificationManager.createNotificationChannel(mChannel)
+
+        manager().createNotificationChannel(mChannel)
+    }
+
+    private fun manager(): NotificationManager {
+        return Res.context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
     }
 }
