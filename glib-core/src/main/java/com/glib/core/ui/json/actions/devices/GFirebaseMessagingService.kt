87e5049ce12305957564349c4fc2c8e4a909229a
Hendrik Gani <hendrik.gani@gmail.com>
2020-09-04 22:28:42 +1000

    Handle onNewToken

--- glib-core/src/main/java/com/glib/core/ui/json/actions/devices/GFirebaseMessagingService.kt
+++ glib-core/src/main/java/com/glib/core/ui/json/actions/devices/GFirebaseMessagingService.kt
@@ -2,22 +2,22 @@ package com.glib.core.ui.json.actions.devices
 
 import android.app.PendingIntent
 import com.glib.core.GApp
+import com.glib.core.http.GRestCallback
 import com.glib.core.json.GJson
 import com.glib.core.json.GJsonObject
 import com.glib.core.logging.GLog
 import com.glib.core.notification.Notifications
+import com.glib.core.ui.ProgressIndicator
+import com.glib.core.ui.json.JsonAction
 import com.glib.core.ui.json.actions.windows.JsonUiScreen
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
-// TODO: OnNewToken should post to server or save it locally or do nothing?
 class GFirebaseMessagingService : FirebaseMessagingService() {
-    private val DEFAULT_CHANNEL_ID = "DEFAULT"
+    private val DEFAULT_CHANNEL_ID = "GLIB_DEFAULT"
     private val DEFAULT_NOTIFICATION_ID = 0
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        GLog.t(javaClass, "onMessageReceived1")
-
         remoteMessage.notification?.let {
             val data = remoteMessage.data
 
@@ -26,14 +26,18 @@ class GFirebaseMessagingService : FirebaseMessagingService() {
             val openUrl = data["openUrl"]
             val onOpen = GJsonObject.Default(data["onOpen"])
 
-//            val onClick = GJsonObject.Default(data["onClick"])
-//            val onClickJson = data["onClick"]
-
-            GLog.t(javaClass, "DATA: ${data}")
-
 //            showNotification(it.title, it.body, channelId, notificationId, onClick)
             showNotification(it.title, it.body, channelId, notificationId, openUrl, onOpen)
         }
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+
+        val callback = GRestCallback.Default(ProgressIndicator.NULL) {
+            // Can't do anything because we don't have access to Activity
+        }
+        GetPushToken.postToServer(token, callback)
     }
 
 //    private fun showNotification(title: String?, body: String?, channelId: String, notificationId: Int, onClick: GJson) {
@@ -55,20 +59,6 @@ class GFirebaseMessagingService : FirebaseMessagingService() {
 //        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 //        notificationManager.notify(0, notificationBuilder.build())
 
-
-        // TODO:
-        // Implement onClick handling
-        // Move to glib
-//        val intent = Intent(this, MainActivity::class.java)
-//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-//        val pendingIntent = PendingIntent.getActivity(this, 0, intent,
-//                PendingIntent.FLAG_ONE_SHOT)
-
-//        val intent = Intent(this, MainActivity::class.java)
-//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-
-//        val intent = JsonUiScreen.intent(onClick)
-////        val intent = GJsonActionDialog.intent(onClick)
         val intent = if (openUrl == null) null else JsonUiScreen.intent(openUrl, false, onOpen)
         val pendingIntent = PendingIntent.getActivity(this, 0, intent,
                 PendingIntent.FLAG_ONE_SHOT)
@@ -80,7 +70,5 @@ class GFirebaseMessagingService : FirebaseMessagingService() {
                 .setContentIntent(pendingIntent)
                 .build()
         Notifications.display(notificationId, notification)
-
-        GLog.t(javaClass, "showNotification1")
     }
 }
--- glib-core/src/main/java/com/glib/core/ui/json/actions/devices/GetPushToken.kt
+++ glib-core/src/main/java/com/glib/core/ui/json/actions/devices/GetPushToken.kt
@@ -1,33 +1,51 @@
 package com.glib.core.ui.json.actions.devices
 
+import android.content.Context
+import com.glib.core.http.*
 import com.glib.core.json.GJson
+import com.glib.core.json.GJsonObject
 import com.glib.core.screen.GActivity
+import com.glib.core.ui.ProgressIndicator
 import com.glib.core.ui.json.JsonAction
+import com.glib.core.utils.Async
+import com.glib.core.utils.GAuth
 import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.iid.FirebaseInstanceId
 import com.google.firebase.iid.InstanceIdResult
 
 class GetPushToken(spec: GJson, screen: GActivity): JsonAction(spec, screen) {
     override fun silentExecute(): Boolean {
-            FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(screen, object : OnSuccessListener<InstanceIdResult?> {
-                override fun onSuccess(instanceIdResult: InstanceIdResult?) {
-                    instanceIdResult?.let { result ->
-                        val paramKey = spec["paramNameForToken"].string ?: "formData[token]"
-                        val properties = spec["onGet"].merge(hashMapOf(paramKey to result.token))
-                        execute(properties, screen, this@GetPushToken)
+        postUrl = spec["postUrl"].string
+        paramKey = spec["paramNameForToken"].string ?: paramKey
+
+        FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(screen, object : OnSuccessListener<InstanceIdResult?> {
+            override fun onSuccess(instanceIdResult: InstanceIdResult?) {
+                instanceIdResult?.let { result ->
+                    val callback = GRestCallback.Default(ProgressIndicator.NULL) { response ->
+                        execute(response.result["onResponse"], screen, this@GetPushToken)
                     }
+                    postToServer(result.token, callback)
+
+//                        val paramKey = spec["paramNameForToken"].string ?: "formData[token]"
+//                        val properties = spec["onGet"].merge(hashMapOf(paramKey to result.token))
+//                        execute(properties, screen, this@GetPushToken)
                 }
-//                fun onSuccess(instanceIdResult: InstanceIdResult) {
-//                    GLog.t(javaClass, "instanceIdResult: ${instanceIdResult.token}")
-//                    val newToken: String = instanceIdResult.getToken()
-//                    DbMap.put(DEVICE_UUID, newToken)
-//                    MyFirebaseMessagingService().onNewToken(newToken)
-//                    TaLog.e(javaClass, "newToken $newToken")
-//                    paramsObject.put("gcm_token", newToken)
-//                    anObject.put("formData", paramsObject)
-//                    Post().execute(activity, anObject)
-//                }
-            })
+            }
+        })
         return true
+    }
+
+    companion object {
+        private var postUrl: String? = null
+        private var paramKey: String = "token"
+
+        fun postToServer(token: String, callback: GRestCallback<GHttpResponse.Default, GRestResponse>) {
+            postUrl?.let {
+                val params = GParams.Default()
+                    .put("authenticity_token", GAuth.csrfToken)
+                    .put(paramKey, token)
+                Rest.POST.asyncUrl(it, params).execute(callback)
+            }
+        }
     }
 }
