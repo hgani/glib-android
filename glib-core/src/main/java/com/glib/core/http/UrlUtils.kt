7de0179d42e642c7f5488f3e168c97f59c81efa2
Hendrik Gani <hendrik.gani@gmail.com>
2022-03-06 18:09:03 +1100

    Implement openWeb action

--- glib-core/src/main/java/com/glib/core/http/UrlUtils.kt
+++ glib-core/src/main/java/com/glib/core/http/UrlUtils.kt
@@ -15,4 +15,36 @@ object UrlUtils {
     fun pathToUrl(path: String): String {
         return "${GHttp.instance.host}${path}"
     }
+    fun jsonToHtml(url: String): String {
+        var newUrl: String
+
+        if (url.indexOf("?") > -1) {
+            newUrl = url.replace(".json?", "?");
+        } else {
+            newUrl = url.replace(".json", "");
+        }
+        // As much as possible, try retaining the front fragment by matching the back, because
+        // the front could be either a `?` or `&`
+        newUrl = newUrl.replace("format=json&", "");
+        // If no match, then we replace the front fragment
+        newUrl = newUrl.replace(Regex("[\\&\\?]format=json"), "");
+
+//        if (originalUrl.indexOf("?") > -1) {
+//            url = originalUrl.replace(/.json\?/, "?");
+//        } else {
+//            url = originalUrl.replace(/.json$/, "");
+//        }
+//        // As much as possible, try retaining the front fragment by matching the back, because
+//        // the front could be either a `?` or `&`
+//        url = url.replace(/format=json\&/, "");
+//        // If no match, then we replace the front fragment
+//        url = url.replace(/[\&\?]format=json/, "");
+//        return url;
+
+//        if (url.indexOf('?') > -1) {
+//
+//        }
+
+        return newUrl
+    }
 }
