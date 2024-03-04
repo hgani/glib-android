794242082a8b1852ea382232ace0c2448c508e45
Hendrik Gani <hendrik.gani@gmail.com>
2024-03-04 15:27:09 +1100

    Comment out broken code

--- glib-core/src/main/java/com/glib/core/ui/json/actions/windows/Open.kt
+++ glib-core/src/main/java/com/glib/core/ui/json/actions/windows/Open.kt
@@ -1,25 +1,8 @@
 package com.glib.core.ui.json.actions.windows
 
-import android.content.Intent
-import android.graphics.drawable.Drawable
-import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import androidx.appcompat.widget.Toolbar
-import com.glib.core.BuildConfig
-import com.glib.core.http.GRestCallback
-import com.glib.core.http.Rest
-import com.glib.core.http.UrlUtils
 import com.glib.core.json.GJson
 import com.glib.core.screen.GActivity
-import com.glib.core.screen.GFragment
-import com.glib.core.screen.GScreenContainer
-import com.glib.core.screen.NavHelper
-import com.glib.core.ui.icon.GIcon
 import com.glib.core.ui.json.JsonAction
-import com.glib.core.ui.json.JsonUi
-import com.glib.core.ui.menu.GMenu
-import com.glib.core.utils.Res
 
 class Open(spec: GJson, screen: GActivity): JsonAction(spec, screen) {
     override fun silentExecute(): Boolean {
--- glib-core/src/main/java/com/glib/core/ui/json/actions/windows/OpenWeb.kt
+++ glib-core/src/main/java/com/glib/core/ui/json/actions/windows/OpenWeb.kt
@@ -1,26 +1,9 @@
 package com.glib.core.ui.json.actions.windows
 
-import android.content.Intent
-import android.graphics.drawable.Drawable
-import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import androidx.appcompat.widget.Toolbar
-import com.glib.core.BuildConfig
-import com.glib.core.http.GRestCallback
-import com.glib.core.http.Rest
 import com.glib.core.http.UrlUtils
 import com.glib.core.json.GJson
-import com.glib.core.logging.GLog
 import com.glib.core.screen.GActivity
-import com.glib.core.screen.GFragment
-import com.glib.core.screen.GScreenContainer
-import com.glib.core.screen.NavHelper
-import com.glib.core.ui.icon.GIcon
 import com.glib.core.ui.json.JsonAction
-import com.glib.core.ui.json.JsonUi
-import com.glib.core.ui.menu.GMenu
-import com.glib.core.utils.Res
 
 class OpenWeb(spec: GJson, screen: GActivity): JsonAction(spec, screen) {
     override fun silentExecute(): Boolean {
